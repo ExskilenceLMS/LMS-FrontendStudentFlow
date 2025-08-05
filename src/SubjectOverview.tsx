9b832d85d5d6de5bf95524434c0bef257afc0ac5
Ranjitha <ranjitha.an.82@gmail.com>
2025-08-05 13:25:40 +0530

    test changes and weekly test update

--- src/SubjectOverview.tsx
+++ src/SubjectOverview.tsx
@@ -449,7 +449,7 @@ const SubjectOverview: React.FC = () => {
                         )}
  
                         <div style={{ fontSize: "12px", cursor: "default", width: '150px' }}>
-                          {day.practiceMCQ && !["Internship", "Semester Exam", "Preparation Day", "Festivals", "Weekly Test","Onsite Workshop"].some(topic => day.topics?.includes(topic)) && (
+                          {day.practiceMCQ && !["Internship", "Semester Exam", "Preparation Day", "Festivals", "Weekly Test","Onsite Workshop","Internals"].some(topic => day.topics?.includes(topic)) && (
                             <>
                               {day.practiceMCQ.questions && (
                                 <p className="m-0 d-flex justify-content-start">
@@ -465,7 +465,7 @@ const SubjectOverview: React.FC = () => {
                           )}
                         </div>
                         <div style={{ fontSize: "12px", cursor: "default", width: '150px' }}>
-                          {day.practiceCoding && !["Internship", "Semester Exam", "Preparation Day", "Festivals", "Weekly Test","Onsite Workshop"].some(topic => day.topics?.includes(topic)) && (
+                          {day.practiceCoding && !["Internship", "Semester Exam", "Preparation Day", "Festivals", "Weekly Test","Onsite Workshop","Internals"].some(topic => day.topics?.includes(topic)) && (
                             <>
                               {day.practiceCoding.questions && (
                                 <p className="m-0 d-flex justify-content-start">
@@ -494,9 +494,17 @@ const SubjectOverview: React.FC = () => {
                         {day.status && (
                           <div>
                             <button
-                              style={{ width: "85px", height: "30px", backgroundColor: "#E9EBFF", boxShadow: "0px 6px 6px rgba(0, 0, 0, 0.3)" }}
+                              style={{ 
+                                width: "85px", 
+                                height: "30px", 
+                                backgroundColor: "#E9EBFF", 
+                                boxShadow: "0px 6px 6px rgba(0, 0, 0, 0.3)",
+                                cursor: (day.status === "Completed" && day.topics?.includes("Weekly Test")) ? "not-allowed" : "pointer",
+                                opacity: (day.status === "Completed" && day.topics?.includes("Weekly Test")) ? 0.6 : 1,
+                              }}
                               className="btn btn-sm"
                               onClick={() => handleStartButtonClickVideo(day.day_key, week.weekNumber, day.status, day.topics)}
+                              disabled={day.status === "Completed" && day.topics?.includes("Weekly Test")}
                             >
                               {day.status}
                             </button>
